e89e12e84353754e499bcfdba38eb863320a6beb
shang yi <shangyilim@gmail.com>
2017-02-26 16:46:15 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,37 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { AddReminderPage } from '../pages/add-reminder/add-reminder';
+import { ReminderDetailPage } from '../pages/reminder-detail/reminder-detail';
+import { ProgressBarComponent } from '../components/progress-bar/progress-bar';
+import { AngularFireModule } from 'angularfire2';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyATI8qTX2EzDpVQiNwtuiUVxxYYEa_MxNI",
+  authDomain: "your-domain-name.firebaseapp.com",
+  databaseURL: "https://ionic2workshop-1e9d2.firebaseio.com/",
+  storageBucket: "your-domain-name.appspot.com",
+  messagingSenderId: '<your-messaging-sender-id>'
+};
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    AddReminderPage
+    AddReminderPage,
+    ProgressBarComponent,
+    ReminderDetailPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    AddReminderPage
+    AddReminderPage,
+    ReminderDetailPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,18 +4,46 @@ import { NavController } from 'ionic-angular';
 
 import { AddReminderPage } from '../add-reminder/add-reminder';
 
+import { ReminderDetailPage } from '../reminder-detail/reminder-detail';
+
+import { Reminder } from '../../classes/reminder';
+
+import { AngularFire} from 'angularfire2';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+  reminderList: Reminder[] = [];
+
+  backgroundColors: string[] = ["#96D096", "#61C8B4", "#D8D685", "#F1B96A"];
+
+  constructor(public navCtrl: NavController, private af: AngularFire) {
+    this.loadReminders();
   }
 
-  navigateToReminder(){
+  navigateToReminder() {
     this.navCtrl.push(AddReminderPage);
   }
+
+  navigateToReminderDetail(reminder: Reminder){
+    this.navCtrl.push(ReminderDetailPage, reminder);
+  }
+
+
+  loadReminders() {
+    this.af.database.list('/reminders').subscribe(result=> {
+
+      this.reminderList = result.map(r=> {
+        let reminder = Object.assign(new Reminder(), r);
+        reminder.key = r.$key;
+        return reminder;
+
+      });
+    })
+  }
+
 
 }
